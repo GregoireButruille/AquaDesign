acb422680c4c2e4720159123ff31df6e643b316e
GregoireButruille <gregoire.butruille@agrocampus-ouest.com>
2021-06-17 11:05:50 +0200

    change shiny_hv_overlap to be able to display best combinations among all

--- R/Shiny_HV_overlap.R
+++ R/Shiny_HV_overlap.R
@@ -12,7 +12,7 @@
 #' @export
 #'
 #' @examples
-Shiny_HV_overlap <- function(nb_combi, selected_abiotics, rescaled_combi_df, species_abiotics_df){
+Shiny_HV_overlap <- function(selected_abiotics, rescaled_combi_df, species_abiotics_df){
 
   nb_combi <- dlg_list(title = "Chose the max number of species in combinations", c(2:(length(hv_list@HVList))))$res
   nb_combi <- as.numeric(nb_combi)
@@ -68,6 +68,7 @@ Shiny_HV_overlap <- function(nb_combi, selected_abiotics, rescaled_combi_df, spe
     ),
 
 
+
     #### set server #####
     server = function(input, output) {
 
@@ -77,37 +78,50 @@ Shiny_HV_overlap <- function(nb_combi, selected_abiotics, rescaled_combi_df, spe
       #static background map
       output$plot1 <- renderPlot({
 
-        if (input$central_species == "None"){
 
-          combi_df_sp <- data.frame(matrix(ncol = as.numeric(input$nb_species), nrow = 0))
-          for (i in 1:length(rescaled_combi_df[,1])){
-            n <- rowSums(rescaled_combi_df == "None")
-            if ((nb_combi - n[i]) == as.numeric(input$nb_species)){
-              vect <- rescaled_combi_df[i,]
-              combi_df_sp <- rbind(combi_df_sp, vect)
-            }
-          }
-          best_combi <- combi_df_sp %>% arrange(desc(as.numeric(combi_df_sp[[nb_combi+1]]))) %>% ## on classe par ordre d?croissant
-            slice(1:input$nb_combi_display) ## on s?lectionne les 5 premi?res lignes (les plus hautes)
 
+        if(input$nb_species == "All"){
+          best_combi <- rescaled_combi_df %>% arrange(desc(as.numeric(rescaled_combi_df[[nb_combi+1]]))) %>% ## sort by index
+            slice(1:input$nb_combi_display) ## keep only best combinations
         }
+
 
         else{
-          row_sub = apply(rescaled_combi_df, 1, function(row) all(row != input$central_species ))
-          combi_df_sub <- rescaled_combi_df[!row_sub,]
+          if (input$central_species == "None"){
 
-          combi_df_central_sp <- data.frame(matrix(ncol = as.numeric(input$nb_species), nrow = 0))
-          for (i in 1:length(combi_df_sub[,1])){
-            n <- rowSums(combi_df_sub == "None")
-            if ((nb_combi - n[i]) == as.numeric(input$nb_species)){
-              vect <- combi_df_sub[i,]
-              combi_df_central_sp <- rbind(combi_df_central_sp, vect)
+            combi_df_sp <- data.frame(matrix(ncol = as.numeric(input$nb_species), nrow = 0))
+            for (i in 1:length(rescaled_combi_df[,1])){
+              n <- rowSums(rescaled_combi_df == "None")
+              if ((nb_combi - n[i]) == as.numeric(input$nb_species)){
+                vect <- rescaled_combi_df[i,]
+                combi_df_sp <- rbind(combi_df_sp, vect)
+              }
             }
+            best_combi <- combi_df_sp %>% arrange(desc(as.numeric(combi_df_sp[[nb_combi+1]]))) %>% ## sort by index
+              slice(1:input$nb_combi_display) ## keep only best combinations
+
           }
 
-          best_combi <- combi_df_central_sp %>% arrange(desc(as.numeric(combi_df_central_sp[[nb_combi+1]]))) %>% ## sort values
-            slice(1:input$nb_combi_display) ## keep the n best combinations to display
+          else{
+            row_sub = apply(rescaled_combi_df, 1, function(row) all(row != input$central_species ))
+            combi_df_sub <- rescaled_combi_df[!row_sub,]
+
+            combi_df_central_sp <- data.frame(matrix(ncol = as.numeric(input$nb_species), nrow = 0))
+            for (i in 1:length(combi_df_sub[,1])){
+              n <- rowSums(combi_df_sub == "None")
+              if ((nb_combi - n[i]) == as.numeric(input$nb_species)){
+                vect <- combi_df_sub[i,]
+                combi_df_central_sp <- rbind(combi_df_central_sp, vect)
+              }
+            }
+
+            best_combi <- combi_df_central_sp %>% arrange(desc(as.numeric(combi_df_central_sp[[nb_combi+1]]))) %>% ## sort values
+              slice(1:input$nb_combi_display) ## keep the n best combinations to display
+          }
+
         }
+
+
 
         names_list <- c()
         for (i in 1:length(best_combi[,1])){
