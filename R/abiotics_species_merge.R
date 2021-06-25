b76dbc3e2f969cb0a8ac237d39f8f3c1b9acf2b3
GregoireButruille <gregoire.butruille@agrocampus-ouest.com>
2021-06-25 13:44:41 +0200

    remove selected_variables as we remove clustering()

--- R/abiotics_species_merge.R
+++ R/abiotics_species_merge.R
@@ -2,20 +2,17 @@
 #'
 #' @param abiotics_df data frame with abiotics data
 #' @param species_df data frame with species occurrences
-#' @param selected_abiotics list of the selected abiotic variables
 #'
 #' @return
 #' @export
 #'
 #' @examples
 
-abiotics_species_merge <- function(abiotics_df, species_df, selected_abiotics) {
-  #select variables from abiotics df to have x and y  (annual_rangeT, annual_meanT, annual_prec_wc, ph_max, maxT_WM, srad_30, prec_DM_wc, prec_seasonality_wc, flow_avg (range?), elevation, slope)
-  selected_abiotics_df <- cbind(abiotics_df[c(1,2)], abiotics_df[selected_abiotics])
+abiotics_species_merge <- function(abiotics_df, species_df) {
 
   #merge and crop datasets to obtain columns as : Species / var 1 / var 2 / ...
-  species_abiotics_df <- data.frame(matrix(ncol = length(names(selected_abiotics_df)), nrow = 0))
-  column_names <- names(selected_abiotics_df)[3:length(names(selected_abiotics_df))]
+  species_abiotics_df <- data.frame(matrix(ncol = length(names(abiotics_df)), nrow = 0))
+  column_names <- names(abiotics_df)[3:length(names(abiotics_df))]
   colnames(species_abiotics_df) <- c("species", column_names)
 
   #set a progress bar
@@ -29,7 +26,7 @@ abiotics_species_merge <- function(abiotics_df, species_df, selected_abiotics) {
     }
 
     species <- species_df[species_df$species==unique(species_df$species)[i],]
-    species_name <- merge(species, selected_abiotics_df, by=c("x","y")) #merge species i with factors data frame
+    species_name <- merge(species, abiotics_df, by=c("x","y")) #merge species i with factors data frame
     colnames(species_name)[3] <- c("species")                          #rename "species name" column
     species_name$species[species_name$species==1] <- as.character(unique(species_df$species)[i]) #replace cell with value = 1 with the species name
     species_name <-na.omit(species_name)                                #remove NA
