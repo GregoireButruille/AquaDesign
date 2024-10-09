89f2ba4fa207b962f4530a8871f7136985f4d888
petroninflorent <116081688+petroninflorent@users.noreply.github.com>
2024-10-09 15:03:40 +0200

    Update FLO1K_select.R

--- R/FLO1K_select.R
+++ R/FLO1K_select.R
@@ -10,37 +10,21 @@ FLO1K_select <- function(resolution) {
 
   flo1k_files_names <- c("av", "mi", "ma")
 
-  #flo1k_files = c()
+  flo1k_files = c()
   
   #for each avg/min/max, open the file as a raster, convert it into data frame and add it to abotics_df
-  #for(i in 1:length(flo1k_files_names)){
-  #  if (resolution == 30){
-   #   name_flow <-paste0("FLO1K.30min.ts.1960.2015.q",flo1k_files_names[i],".nc")
-   #   flow <-raster(unzip("flow_30_zip.zip",name_flow))
-   # }
-   # if (resolution == 10){
-   #   name_flow <-paste0("FLO1K.5min.ts.1960.2015.q",flo1k_files_names[i],".nc")
-   #   flow <-raster(unzip("flow_5_zip.zip",name_flow))
-   # }
-   # flo1k_files = c(flo1k_files, flow)
-  #}
-
-  flo1k_files = list()
-  #unzips
   for(i in 1:length(flo1k_files_names)){
     if (resolution == 30){
       name_flow <-paste0("FLO1K.30min.ts.1960.2015.q",flo1k_files_names[i],".nc")
-      unzip("flow_30_zip.zip",name_flow)
-      flo1k_files <- c(flo1k_files, name_flow)
+      flow <-raster(unzip("flow_30_zip.zip",name_flow))
     }
     if (resolution == 10){
       name_flow <-paste0("FLO1K.5min.ts.1960.2015.q",flo1k_files_names[i],".nc")
-      unzip("flow_5_zip.zip",name_flow)
-      flo1k_files <- c(flo1k_files, name_flow)
+      flow <-raster(unzip("flow_5_zip.zip",name_flow))
     }
+    flo1k_files = c(flo1k_files, flow)
   }
-  
-  #result <- flo1k_files
-  result <- stack(flo1k_files)
+
+  result <- flo1k_files
   return(result)
 }
